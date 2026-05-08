407527fa15ae418a4df90b71bd544498ac6aa7f3
Nicolas Gonzalez <ndgfrs@gmail.com>
2026-05-08 16:57:35 -0400

    better error message for missing strkey (#1)

--- src/util.rs
+++ src/util.rs
@@ -36,16 +36,18 @@ pub fn expand_vars(value: &str, strings: &Section) -> Result<String, ExpandVarsE
             .entries()
             .iter()
             .find_map(|entry| match entry {
-                Entry::Item(key, value) if var_lowercase == key.to_lowercase() => match value {
-                    Value::Raw(s) => Some(s.as_str()),
-                    // TODO: [Strings] section is special and should not be allowed to have
-                    // Value::List. Not an urgent problem since we are only reading INF files,
-                    // but this needs to be fixed if we ever want to implement an INF writer.
-                    Value::List(..) => None,
-                },
+                Entry::Item(key, value) if var_lowercase == key.to_lowercase() => {
+                    match value {
+                        Value::Raw(s) => Some(s.as_str()),
+                        // TODO: [Strings] section is special and should not be allowed to have
+                        // Value::List. Not an urgent problem since we are only reading INF files,
+                        // but this needs to be fixed if we ever want to implement an INF writer.
+                        Value::List(..) => None,
+                    }
+                }
                 _ => None,
             })
-            .ok_or(ExpandVarsError::NotFound)?;
+            .ok_or(ExpandVarsError::NotFound(var))?;
 
         result.push_str(replacement);
     }
@@ -53,10 +55,10 @@ pub fn expand_vars(value: &str, strings: &Section) -> Result<String, ExpandVarsE
     Ok(result)
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ExpandVarsError {
     Unterminated,
-    NotFound,
+    NotFound(String),
 }
 
 impl std::error::Error for ExpandVarsError {
@@ -69,7 +71,7 @@ impl fmt::Display for ExpandVarsError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
             Self::Unterminated => "unterminated %strkey% sequence".fmt(f),
-            Self::NotFound => "string key not found".fmt(f),
+            Self::NotFound(ref strkey) => write!(f, "key {strkey:?} not found in [Strings]"),
         }
     }
 }
@@ -132,5 +134,21 @@ mod tests {
         let result = expand_vars("%unterminated", &strings);
 
         assert!(matches!(result, Err(ExpandVarsError::Unterminated)));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let strings = Section::new(
+            "Strings".to_owned(),
+            vec![Entry::Item(
+                "name".to_owned(),
+                Value::Raw("Stinky".to_owned()),
+            )],
+        );
+
+        let expanded =
+            expand_vars("Hello, %Name%!", &strings).expect("expected hardcoded string to be valid");
+
+        assert_eq!(expanded, "Hello, Stinky!".to_owned());
     }
 }
